323ca6d923d7a3b8e4ea953ee8d6c957e254c6e0
Humberto Rocha <humrochagf@gmail.com>
2025-04-16 22:13:20 -0400

    Codigo fonte recuperado e adaptado to planet python

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Repo público para atualização da lista de sites do https://planet.python.org.br/
 
+Recuperado do repo planet python https://github.com/python/planet
+
 ## Adição de Feeds:
 
-Para adicionar um feed, faça um PR com a adição no arquivo config/planet.conf
+Para adicionar um feed, faça um PR com a adição no arquivo config/config.ini
 
 ## Template
 
---
+++ config/sort-ini.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+from configparser import DEFAULTSECT, RawConfigParser
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = "config.ini"
+
+oconfig = RawConfigParser()
+oconfig.read(filename)
+
+# This part will destroy the configuration if there's a crash while
+# writing the output.  We're in an GIT-controlled directory, so
+# I didn't care enough to fix this.
+with open(filename, "w") as fd:
+    # Copy of write() code that sorts output by section
+    if oconfig._defaults:
+        fd.write("[%s]\n" % DEFAULTSECT)
+        for key, value in oconfig._defaults.items():
+            fd.write("{} = {}\n".format(key, str(value).replace("\n", "\n\t")))
+        fd.write("\n")
+
+    result = {}
+    for section in sorted(oconfig._sections):
+        if section == "Planet":
+            fd.write("[%s]\n" % section)
+        for key, value in oconfig._sections[section].items():
+            if key != "__name__":
+                if section == "Planet":
+                    fd.write("{} = {}\n".format(key, str(value).replace("\n", "\n\t")))
+                else:
+                    result[value.replace('"', "")] = section
+        if section == "Planet":
+            fd.write("\n")
+
+    for key, value in sorted(result.items()):
+        fd.write("[%s]\n" % value)
+        name = key
+        if "'" in key:
+            name = '"%s"' % key
+        fd.write("name = %s\n" % name)
+        fd.write("\n")
